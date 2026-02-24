70b0df3d62b1de26cf9228f89e61ec6c1af44d8f
Victor Derks <vbaderks@gmail.com>
2026-02-24 22:56:56 +0100

    add bottles

--- Formula/charls.rb
+++ Formula/charls.rb
@@ -5,6 +5,12 @@ class Charls < Formula
   sha256 "d1c2c35664976f1e43fec7764d72755e6a50a80f38eca70fcc7553cad4fe19d9"
   license "BSD-3-Clause"
 
+  bottle do
+    root_url "https://github.com/team-charls/homebrew-tap/releases/download/charls-2.4.2"
+    sha256 cellar: :any, arm64_sonoma:   "a54525ff53cb07faf40ebc23ca57c2348dbc72175112c49865d4556a5c0f760f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "16bdb51beb4b6ffb2bf06e1c1a1037939c1169fefcf5c721c5fdf91df86d58b0"
+  end
+
   depends_on "cmake" => :build
 
   def install
